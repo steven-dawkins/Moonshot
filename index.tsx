f8c3a0edf97c4a868786b081b6cb7cabcbcb304e
Steven Dawkins <sdawkins@oxfordeconomics.com>
2020-11-02 23:37:32 +0000

    setup react typescript template

---
+++ index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { render } from "react-dom";
+
+const el = document.getElementById("body");
+
+render(<div>
+        <h1>Welcome</h1>
+       </div>,
+    el);
