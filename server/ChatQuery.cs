485311463cbe46c93def52e9d606802e8f702cb7
Steven Dawkins <sdawkins@oxfordeconomics.com>
2020-11-19 00:28:08 +0000

    added player entity to server

--- server/ChatQuery.cs
+++ server/ChatQuery.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using GraphQL.Types;
+using Moonshot_Server.MoonSchema.GraphQLTypes;
 
 namespace Moonshot_Server
 {
@@ -9,7 +10,7 @@ namespace Moonshot_Server
         {
             Field<ListGraphType<StringGraphType>>("messages", resolve: context => chat.AllMessages.Take(100));
 
-            Field<ListGraphType<StringGraphType>>("players", resolve: context => chat.Players.Take(100));
+            Field<ListGraphType<PlayerGraphType>>("players", resolve: context => chat.Players.Take(100));
         }
     }
 }
---
+++ server/MoonSchema/GraphQLTypes/PlayerGraphType.cs
@@ -0,0 +1,12 @@
+﻿using GraphQL.Types;
+
+namespace Moonshot_Server.MoonSchema.GraphQLTypes
+{
+    public class PlayerGraphType : ObjectGraphType<Player>
+    {
+        public PlayerGraphType()
+        {
+            _ = Field(x => x.Name, type: typeof(StringGraphType));
+        }
+    }
+}
